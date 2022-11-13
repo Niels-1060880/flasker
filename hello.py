1214acb035ef074aa4fdcfa3f16e5f45c96ae92c
Niels van den Bosch <113016937+Niels-1060880@users.noreply.github.com>
2022-11-13 13:39:47 +0100

    Made a form

--- hello.py
+++ hello.py
@@ -1,7 +1,16 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 # Create a Flask Instance
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "my super secret key"
+
+# Create a Form Class
+class NamerForm(FlaskForm):
+    name = StringField("What's Your Name?", validators=[DataRequired()])
+    submit = SubmitField("Submit")
 
 # Create a route decorator
 
@@ -34,9 +43,23 @@ def user(name):
 def page_not_found(e):
     return render_template("404.html"), 404
 
-# Internal server error
+# Internal Server Error
 
 
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template("404.html"), 500
+
+
+# Create Name Page
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NamerForm()
+    # Validate Form
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.date = ''
+    return render_template("name.html",
+                            name = name,
+                            form = form)
